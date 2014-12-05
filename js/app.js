f2051d5ce711de0b6132717af0bd5e9c2e25b9a2
David <davidroth0820@gmail.com>
2014-12-04 17:37:14 -0700

    Fixed commenting and semicolon issues pointed out during instructor review.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 var enemyPosY = [60, 143, 226];
 var enemySpeed = [25,100,125,135,150,250];
 
-// Enemies our player must avoid
+/** Enemies our player must avoid */
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    /** Variables applied to each of our instances go here,
+     * we've provided one for you to get started
+     *The image/sprite for our enemies, this uses
+     *a helper we've provided to easily load images
+     */
     this.sprite = 'images/enemy-bug.png';
     this.x = -50;
     this.y = enemyPosY[Math.floor(Math.random() * 3)];
@@ -18,12 +18,14 @@ var reset = function(){
   player.x = 200;
   player.y = 392;
 };
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/**Update the enemy's position, required method for game
+ * Parameter: dt, a time delta between ticks
+ */
 Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
+  /**You should multiply any movement by the dt parameter
+   * which will ensure the game runs at the same speed for
+   * all computers.
+    */
   this.x = this.x + (this.speed * dt);
   if (this.x > 550) {
     this.x = -100;
@@ -55,8 +57,9 @@ Enemy.prototype.update = function(dt) {
     this.tileX = 1;
   }
 
-  //Check for collision.
-  //Game will reset and dieSound will play if collision occurrs.
+  /**Check for collision.
+   * Game will reset and dieSound will play if collision occurrs.
+   */
   if (
     player.x <=(this.x + 75)
     && this.x <=(player.x + 75)
@@ -66,21 +69,21 @@ Enemy.prototype.update = function(dt) {
     dieSound();
     reset();
   }
-}
+};
 
-// Draw the enemy on the screen, required method for game
+/** Draw the enemy on the screen, required method for game */
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+/** Now write your own player class
+ *  This class requires an update(), render() and a handleInput() method.
+ */  
 var Player = function(){
     this.sprite = 'images/char-boy.png';
     this.x = 200;
     this.y = 392;
-}
+};
 
 Player.prototype.update = function(){
   if (this.ctrlKey === 'left' && this.x != 0) {
@@ -102,40 +105,40 @@ Player.prototype.update = function(){
     winSound();
     this.reset();
   }
-}
+};
 
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 Player.prototype.handleInput = function(key) {
   this.ctrlKey = key;
-}
+};
 
 Player.prototype.reset = function() {
   this.x = 200;
   this.y = 392;
-}
+};
 
-//Sound plays when user sucessfully crosses the road
+/** Sound plays when user sucessfully crosses the road */
 function winSound (){
   var win = new Audio("audio/win.wav")
   win.play();
 }
 
-//Sound plays when user collides with bug
+/** Sound plays when user collides with bug */
 function dieSound () {
   var splat = new Audio("audio/splat.wav");
   splat.play();
 }
 
-//Sound plays when directional arrows are clicked
+/** Sound plays when directional arrows are clicked */
 function moveSound () {
   var snd = new Audio("audio/move.wav");
     snd.play();
   }
 
-//Handles reset button for game
+/** Handles reset button for game */
 function gameReset () {
   var btn = document.getElementById("resetGame");
   if(btn.onclick) {
@@ -143,9 +146,10 @@ function gameReset () {
   }
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/** Now instantiate your objects.
+ * Place all enemy objects in an array called allEnemies
+ * Place the player object in a variable called player
+ */
 var enemy01 = new Enemy();
 var enemy02 = new Enemy();
 var enemy03 = new Enemy();
@@ -157,8 +161,9 @@ var player = new Player();
 
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/** This listens for key presses and sends the keys to your
+ * Player.handleInput() method. You don't need to modify this.
+ */
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
